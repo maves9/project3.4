1de9f50fe1026d670066b8a137237119bc39f9e1
Martin vestergaard <mvmartinxd@gmail.com>
2017-11-30 12:18:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Counter</title>
+<link href="css/main.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <main id="main">
+        <h1>Hi world</h1>
+
+    </main>
+
+    <script src="js/functions.js"></script>
+</body>
+</html>

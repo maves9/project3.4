b7523015192274a62ca0e15f68a822559915b1b0
Henriette Jørgensen <31208209+HenrMDU@users.noreply.github.com>
2017-12-13 13:34:57 +0100

    Hjem siden

---
+++ hjem/include/header.php
@@ -0,0 +1,34 @@
+<!-- Mobil nav slide ind fra siden
+https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+(.sidenav ændre left:0; til right:0;)
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_full -->
+
+<header>
+  <section id="header__top">
+    <p id="title">Jagt & Fiskerimagasinet</p>
+
+    <nav id="header__top--nav">
+      <ul>
+        <li id="nak-btn">NAK ET DYR</li>
+        <li><img class="header__top--icon" src="img/icon/indkøbskurv.svg" alt="Indkøbskurv">Indkøbskurv</li>
+        <li><img class="header__top--icon" src="img/icon/din-konto.svg" alt="Din konto">Din konto</li>
+      </ul>
+    </nav>
+
+    <img id="mobil_menu" src="img/icon/mobil-menu.svg" alt="Mobil Menu">
+  </section>
+
+  <section id="header__bottom">
+    <img id="logo" src="img/logo-dark-bg.svg" alt="Jagt & Fiskerimagasinet">
+
+    <nav id="header__bottom--nav">
+      <ul>
+        <li>Hjem</li>
+        <li>Webshop</li>
+        <li>Om os</li>
+        <li>Kontakt</li>
+      </ul>
+    </nav>
+  </section>
+</header>
---
+++ hjem/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Jagt & Fiskerimagasinet Aarhus - Hjem</title>
+
+  <link rel="stylesheet" href="css/hjem.css">
+  <link rel="stylesheet" href="css/hjem-media.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Spectral+SC:700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
+</head>
+
+<body>
+<?php include 'include/header.php'; ?>
+
+<main>
+  <div id="main__video">
+    <iframe id="main__video--iframe" src="https://www.youtube.com/embed/kDJl8wDb7t0?autoplay=1&controls=0&loop=1&rel=0&amp;controls=0&amp;showinfo=0;" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+
+    <article class="main__webshop">
+      <section class="main__webshop--outdoor"><h2>OUTDOOR</h2></section>
+      <section class="main__webshop--fiskeri"><h2>FISKERI</h2></section>
+      <section class="main__webshop--jagt"><h2>JAGT</h2></section>
+    </article>
+  </div>
+
+  <article id="main__events">
+    <section id="main__events--nak">
+      <img class="main__events--img" src="img/jørgen-skoubo.png" alt="Nak & Æd">
+      <section class="main__events--content">
+        <h2>NAK & ÆD -<br>FOREDRAG MED JØRGEN SKOUBO</h2>
+        <a href="#">Læs mere her</a>
+      </section>
+      <section class="main__events--date"><p>28. december</p><p>2017</p>
+      </section>
+    </section>
+
+    <section id="main__events--fisketur">
+      <img class="main__events--img" src="img/fisketur.png" alt="Nak & Æd">
+      <section class="main__events--content">
+        <h2>NAK & ÆD -<br>FOREDRAG MED JØRGEN SKOUBO</h2>
+        <a href="#">Læs mere her</a>
+      </section>
+      <section class="main__events--date"><p>28. december</p><p>2017</p></section>
+    </section>
+  </article>
+
+  <div id="main__social">
+    <section id="main__social--facebook">
+      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FJagtOgFiskerimagasinetAarhus%3Ffref%3Dts&tabs=timeline%2C%20events%2C%20messages&width=435&height=430&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="435" height="430" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    </section>
+
+    <section id="main__social--youtube">
+      <h3>Vidste du at vi er på YouTube?</h3>
+      <p>Besøg vores YouTube kanal og få tips og triks til din hobby</p>
+      <iframe width="331" height="186" src="https://www.youtube.com/embed/FscVkAwN6ps" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+    </section>
+  </div>
+</main>
+
+<?php include 'include/footer.php'; ?>
+</body>
+</html>
